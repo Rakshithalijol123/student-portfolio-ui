a2df2da4705ed24cc8ea3c6f06f96d5adf03271e
Rakshit Halijol <20beam031@dsce.edu.in>
2023-05-23 22:58:47 +0530

    Login page is added

---
+++ src/app/login/loading.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div className="flex text- justify-center items-center h-screen">
+      <span className="text-6xl font-bold text-violet-500">Performance</span>
+      <span className="text-6xl font-bold text-black">Xcel</span>
+      <span className="text-6xl font-bold text-white">.......</span>
+    </div>
+  );
+};
+
+export default loading;
---
+++ src/app/login/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import InputForm from "./InputForm";
+
+const page = () => {
+  return (
+    <div>
+      <InputForm />
+    </div>
+  );
+};
+
+export default page;
