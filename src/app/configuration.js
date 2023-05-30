772e6743d33e9c3c4647e74a22783e3da93bda81
Rakshit Halijol <20beam031@dsce.edu.in>
2023-05-30 18:06:23 +0530

    Updated configuration file

--- src/app/configuration.js
+++ src/app/configuration.js
@@ -1,4 +1,13 @@
-// src/config.js
-const API_BASE_URL = "http://localhost:8000";
+// // src/config.js
+// const API_BASE_URL = "http://localhost:8000";
 
-export { API_BASE_URL };
+// export { API_BASE_URL };
+
+export const username =
+  "547353cb38f248482ee2d404ab38ad5a16bd073bd023efe0fcad693e6936eb4c";
+export const password =
+  "d8ae0c4a2cac77d554136dae65a4e36cb63e990dbd5b9bfc22ca2b7d0dc50a7d";
+
+// export const baseurl = "http://localhost:8000";
+
+export const baseurl = "http://65.2.148.228";
