0f9bfccce111daeac87ba47a09cfbe9f66eda5b2
Rakshit Halijol <20beam031@dsce.edu.in>
2023-05-30 19:02:10 +0530

    Added register button

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,16 @@
+"use client";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+  const goToRegister = () => {
+    router.push("/register");
+  };
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>Student portfolio app under construction...</div>
+      <button onClick={goToRegister}>Register</button>
     </main>
   );
 }
